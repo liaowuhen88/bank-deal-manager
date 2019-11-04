50707e6cf08e4780892c132e4b14d39f2103b605
zhangwenchao19 <zhangwenchao19@jd.com>
2019-11-04 11:39:04 +0800

    打包为war文件，支持tomcat部署

--- src/main/java/com/wwt/managemail/ManagemailApplication.java
+++ src/main/java/com/wwt/managemail/ManagemailApplication.java
@@ -2,14 +2,21 @@ package com.wwt.managemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.wwt.managemail.mapper")
-public class ManagemailApplication {
+public class ManagemailApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagemailApplication.class, args);
     }
-
+    // 用于构建war文件并进行部署
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
