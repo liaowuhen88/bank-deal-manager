19e712a7bd723738f3a87edd318e74e1cdd25fcb
zhangwenchao19 <zhangwenchao19@jd.com>
2019-11-05 10:13:41 +0800

    新增产品列表

--- src/main/java/com/wwt/managemail/controller/BankController.java
+++ src/main/java/com/wwt/managemail/controller/BankController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,8 +19,20 @@ public class BankController extends BaseController{
     @Autowired
     BankService bankService;
 
+    @PostMapping("getBankName")
+    public Result<String> getBankName() {
+        List<String> list = new ArrayList<>();
+        list.add("招商银行");
+        list.add("交通银行");
+        list.add("建设银行");
+        list.add("天津银行");
+        list.add("工商银行");
+        list.add("中信银行");
+        return Result.sucess(list);
+    }
+
     @PostMapping("insert")
-    public Result<Integer> insert(@RequestBody Bank bank){
+    public Result<Integer> insert(@RequestBody Bank bank) {
         int code = bankService.insert(bank);
         return Result.sucess(code);
     }
--- src/main/java/com/wwt/managemail/entity/BankProduct.java
+++ src/main/java/com/wwt/managemail/entity/BankProduct.java
@@ -11,19 +11,39 @@ public class BankProduct {
     @Id
     @KeySql(useGeneratedKeys = true)
     private Integer id;
+    /**
+     * 银行
+     */
     private String bank;
-
+    /**
+     * 产品类型
+     */
     private String productType;
-
+    /**
+     * 产品
+     */
     private String bankProduct;
-
+    /**
+     * 预期利率
+     */
     private Long expectedinterestRate;
-
+    /**
+     * 付息方式
+     */
     private String interestPaymentMethod;
-
+    /**
+     * 存款期（日）
+     */
     private Integer depositPeriod;
+    /**
+     * 备注
+     */
     private String remark;
 
     private String creator;
     private Date creatorTime;
+
+    public String getSelectName() {
+        return productType + "_" + bankProduct;
+    }
 }
