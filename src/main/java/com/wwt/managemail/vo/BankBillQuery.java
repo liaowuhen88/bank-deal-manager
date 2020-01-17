3c3a8c4cb65bef9112e96a8bb77d664c5e05f85a
zhangwenchao19 <zhangwenchao19@jd.com>
2020-01-17 15:53:45 +0800

    前端发版

--- src/main/java/com/wwt/managemail/vo/BankBillQuery.java
+++ src/main/java/com/wwt/managemail/vo/BankBillQuery.java
@@ -2,6 +2,7 @@ package com.wwt.managemail.vo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -13,22 +14,11 @@ public class BankBillQuery extends CommonSearchVO {
     // 产品id
     private Integer myProductId;
     private int[] transactionTypes;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonFormat(pattern = "yyyy-MM-dd")
-    private Date[] times;
-
-    public Date getStartTime() {
-        if (null != times && times.length == 2) {
-            return times[0];
-        }
-        return startTime;
-    }
-
-    public Date getEndTime() {
-        if (null != times && times.length == 2) {
-            return times[1];
-        }
-        return endTime;
-    }
     private Date startTime;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date endTime;
 }
